71daf19bd3ae8209c0b0a8e32a5033be1c6872ad
Roman <sholomitskyR@yandex.by>
2021-09-15 20:39:32 +0300

    feat:solve carbon-datting

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,6 +3,7 @@ import { NotImplementedError } from '../extensions/index.js';
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
 
+
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
@@ -17,7 +18,13 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+export default function dateSample(sampleActivity) {
+  const regexp = /^[1-9]\d*\.\d*|0\.\d*[1-9]\d*$|^(\-|\+)?\d+(\.\d+)?$/
+  if (typeof sampleActivity !== 'string') {return false}
+  if (sampleActivity <= 0 || sampleActivity > 15) {return false}
+  if (sampleActivity.length === 0) {return false}
+  
+  if (regexp.test(sampleActivity) === false) {return false}
+
+   return (Math.ceil(Math.log(MODERN_ACTIVITY / + sampleActivity) / (0.693 / HALF_LIFE_PERIOD)))
+  }
